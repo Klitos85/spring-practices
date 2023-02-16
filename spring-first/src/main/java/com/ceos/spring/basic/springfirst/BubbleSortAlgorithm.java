ba435d81bd6e38370801f3254797976321fbd14f
Carlos Ortiz <cortiz@thoughtworks.com>
2023-02-16 16:41:02 -0500

    feat: Add Component and Autowired spring notations and set logging

--- spring-first/src/main/java/com/ceos/spring/basic/springfirst/BubbleSortAlgorithm.java
+++ spring-first/src/main/java/com/ceos/spring/basic/springfirst/BubbleSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.ceos.spring.basic.springfirst;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubbleSortAlgorithm implements SortAlgorithm {
   public int[] sort(int[] numbers) {
     return numbers;
--- spring-first/src/main/java/com/ceos/spring/basic/springfirst/SpringFirstApplication.java
+++ spring-first/src/main/java/com/ceos/spring/basic/springfirst/SpringFirstApplication.java
@@ -2,17 +2,21 @@ package com.ceos.spring.basic.springfirst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringFirstApplication {
 
   public static void main(String[] args) {
 
-    BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+    // We don't need to create a new instance of a Component
+    // BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+
+    ApplicationContext applicationContext =  SpringApplication.run(SpringFirstApplication.class, args);
+
+    BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+    
     int result = binarySearch.binarySearch(new int[] { 124, 6 }, 3);
     System.out.println("Result: " + result);
-    
-    SpringApplication.run(SpringFirstApplication.class, args);
   }
-
 }
