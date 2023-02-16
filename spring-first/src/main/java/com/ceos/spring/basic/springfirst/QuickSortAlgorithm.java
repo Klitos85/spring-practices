f1a36b40ba31890ba202cc6abd62c38ec3bcb929
Carlos Ortiz <cortiz@thoughtworks.com>
2023-02-16 17:16:49 -0500

    feat: Add @Primary to prioritary class

--- spring-first/src/main/java/com/ceos/spring/basic/springfirst/QuickSortAlgorithm.java
+++ spring-first/src/main/java/com/ceos/spring/basic/springfirst/QuickSortAlgorithm.java
@@ -1,5 +1,7 @@
 package com.ceos.spring.basic.springfirst;
+import org.springframework.stereotype.Component;
 
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm {
   public int[] sort(int[] numbers) {
     return numbers;
