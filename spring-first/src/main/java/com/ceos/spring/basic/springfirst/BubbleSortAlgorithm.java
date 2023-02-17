7926bb12289ad1a88fdc7b81135795bca2ef3574
Carlos Ortiz <cortiz@thoughtworks.com>
2023-02-17 09:51:33 -0500

    feat: Autowiring by name

--- spring-first/src/main/java/com/ceos/spring/basic/springfirst/BubbleSortAlgorithm.java
+++ spring-first/src/main/java/com/ceos/spring/basic/springfirst/BubbleSortAlgorithm.java
@@ -1,10 +1,7 @@
 package com.ceos.spring.basic.springfirst;
-
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm {
   public int[] sort(int[] numbers) {
     return numbers;
